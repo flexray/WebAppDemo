9d75f518b51986465142ae90b090ea5d997bd53f
Maciej Stopa <mastopa@microsoft.com>
2016-01-29 13:42:02 +0100

    FirstCommit

---
+++ WebAppDemo1/WebAppDemo1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppDemo1.Startup))]
+namespace WebAppDemo1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
